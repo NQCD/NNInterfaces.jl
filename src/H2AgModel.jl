f0bfd4826d5269fa191c65a93cecd09a163937d7
James Gardner <james.gardner1421@gmail.com>
2021-06-04 12:21:50 +0100

    Fix unit conversions

--- src/H2AgModel.jl
+++ src/H2AgModel.jl
@@ -26,25 +26,30 @@ struct H2AgModel{T} <: AdiabaticFrictionModel
 end
 
 function NonadiabaticModels.potential!(model::H2AgModel, V::AbstractVector, R::AbstractMatrix)
-    @views model.tmp_coordinates .= au_to_ang.(R[:,model.h2indices])
+    set_coordinates!(model, R)
     ccall(((:pot0_, h2ag111_pes)), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
             2, model.tmp_coordinates, model.tmp_energy)
-    V[1] = eV_to_au(model.tmp_energy[1])
+    V[1] = austrip(model.tmp_energy[1]*u"eV")
 end
 
 function NonadiabaticModels.derivative!(model::H2AgModel, D::AbstractMatrix, R::AbstractMatrix)
-    @views model.tmp_coordinates .= au_to_ang.(R[:,model.h2indices])
+    set_coordinates!(model, R)
     ccall((:dpeshon_, h2ag111_pes), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
             2, model.tmp_coordinates, D)
-    D .= eV_per_ang_to_au.(D)
+    @. D = austrip(D*u"eV/Å")
 end
 
 function NonadiabaticModels.friction!(model::H2AgModel, F::AbstractMatrix, R::AbstractMatrix)
-    @views model.tmp_friction_coordinates .= au_to_ang.(R[:,model.h2indices])
+    set_coordinates!(model, R)
     cd(splitdir(H2AgModel_jll.h2ag111_friction_path)[1]) do
         ccall((:tensor_, h2ag111_friction), Cvoid, (Ref{Float64}, Ptr{Float64}),
               model.tmp_coordinates, model.tmp_friction)
     end
-    F[1:6,1:6] .= ps_inv_to_au.(model.tmp_friction)
-    F .*= austrip(elements[:H].atomic_mass)
+    @. F[1:6,1:6] = austrip(model.tmp_friction*u"ps^-1")
+    @. F *= austrip(elements[:H].atomic_mass)
+end
+
+function set_coordinates!(model::H2AgModel, R)
+    @views model.tmp_coordinates .= ustrip.(auconvert.(u"Å", R[:,model.h2indices]))
+    return nothing
 end
--- src/NNInterfaces.jl
+++ src/NNInterfaces.jl
@@ -1,6 +1,7 @@
 module NNInterfaces
 
 using Reexport
+using Unitful, UnitfulAtomic
 @reexport using NonadiabaticModels
 
 include("H2AgModel.jl")
