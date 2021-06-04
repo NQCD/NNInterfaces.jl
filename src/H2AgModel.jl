1d669fac70c8232b027e7be2b713f45d4a2e1731
James Gardner <james.gardner1421@gmail.com>
2021-06-04 11:56:31 +0100

    Fix renaming

--- src/H2AgModel.jl
+++ src/H2AgModel.jl
@@ -25,21 +25,21 @@ struct H2AgModel{T} <: AdiabaticFrictionModel
     end
 end
 
-function potential!(model::EANN_H₂Ag, V::AbstractVector, R::AbstractMatrix)
+function potential!(model::H2AgModel, V::AbstractVector, R::AbstractMatrix)
     @views model.tmp_coordinates .= au_to_ang.(R[:,model.h2indices])
     ccall(((:pot0_, h2ag111_pes)), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
             2, model.tmp_coordinates, model.tmp_energy)
     V[1] = eV_to_au(model.tmp_energy[1])
 end
 
-function derivative!(model::EANN_H₂Ag, D::AbstractMatrix, R::AbstractMatrix)
+function derivative!(model::H2AgModel, D::AbstractMatrix, R::AbstractMatrix)
     @views model.tmp_coordinates .= au_to_ang.(R[:,model.h2indices])
     ccall((:dpeshon_, h2ag111_pes), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
             2, model.tmp_coordinates, D)
     D .= eV_per_ang_to_au.(D)
 end
 
-function friction!(model::EANN_H₂Ag, F::AbstractMatrix, R::AbstractMatrix)
+function friction!(model::H2AgModel, F::AbstractMatrix, R::AbstractMatrix)
     @views model.tmp_friction_coordinates .= au_to_ang.(R[:,model.h2indices])
     cd(h2ag111_friction_path) do
         ccall((:tensor_, h2ag111_friction), Cvoid, (Ref{Float64}, Ptr{Float64}),
