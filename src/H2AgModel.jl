8722033f71bfc788714208eedd759f8d59c7ffd5
James Gardner <james.gardner1421@gmail.com>
2021-09-22 15:43:49 +0100

    Update compatibility with NonadiabaticModels 0.5

--- src/H2AgModel.jl
+++ src/H2AgModel.jl
@@ -1,6 +1,9 @@
 
-using PeriodicTable
-using H2AgModel_jll
+using Unitful: @u_str, ustrip
+using UnitfulAtomic: austrip, auconvert
+using PeriodicTable: PeriodicTable
+using H2AgModel_jll: H2AgModel_jll
+using NonadiabaticModels: FrictionModels
 
 export H2AgModel
 
@@ -8,7 +11,7 @@ export H2AgModel
 J. Phys. Chem. Lett. 2019, 10, 4962−4967
 J. Phys. Chem. C 2020, 124, 186−195
 """
-struct H2AgModel{T} <: AdiabaticFrictionModel
+struct H2AgModel{T} <: FrictionModels.AdiabaticFrictionModel
     h2indices::Vector{Int}
     tmp_coordinates::Matrix{T}
     tmp_friction_coordinates::Matrix{T}
@@ -17,7 +20,7 @@ struct H2AgModel{T} <: AdiabaticFrictionModel
     function H2AgModel(h2indices=[1, 2])
 
         cd(splitdir(H2AgModel_jll.h2ag111_pes_path)[1]) do
-            ccall((:pes_init_, h2ag111_pes), Cvoid, ())
+            ccall((:pes_init_, H2AgModel.h2ag111_pes), Cvoid, ())
         end
 
         @assert h2indices == [1, 2]
@@ -27,27 +30,27 @@ end
 
 function NonadiabaticModels.potential(model::H2AgModel, R::AbstractMatrix)
     set_coordinates!(model, R)
-    ccall(((:pot0_, h2ag111_pes)), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
+    ccall(((:pot0_, H2AgModel.h2ag111_pes)), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
             2, model.tmp_coordinates, model.tmp_energy)
     return austrip(model.tmp_energy[1]*u"eV")
 end
 
 function NonadiabaticModels.derivative!(model::H2AgModel, D::AbstractMatrix, R::AbstractMatrix)
     set_coordinates!(model, R)
-    ccall((:dpeshon_, h2ag111_pes), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
+    ccall((:dpeshon_, H2AgModel.h2ag111_pes), Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
             2, model.tmp_coordinates, D)
     @. D = austrip(D*u"eV/Å")
     return D
 end
 
-function NonadiabaticModels.friction!(model::H2AgModel, F::AbstractMatrix, R::AbstractMatrix)
+function FrictionModels.friction!(model::H2AgModel, F::AbstractMatrix, R::AbstractMatrix)
     set_coordinates!(model, R)
     cd(splitdir(H2AgModel_jll.h2ag111_friction_path)[1]) do
-        ccall((:tensor_, h2ag111_friction), Cvoid, (Ref{Float64}, Ptr{Float64}),
+        ccall((:tensor_, H2AgModel.h2ag111_friction), Cvoid, (Ref{Float64}, Ptr{Float64}),
               model.tmp_coordinates, model.tmp_friction)
     end
     @. F[1:6,1:6] = austrip(model.tmp_friction*u"ps^-1")
-    @. F *= austrip(elements[:H].atomic_mass)
+    @. F *= austrip(PeriodicTable.elements[:H].atomic_mass)
     return F
 end
 
